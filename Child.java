2ef764a125a3438e4e498e8df8fe5c6910dcdb99
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-03-22 11:03:29 -0400

    Add files via upload
    Initial child class

---
+++ Child.java
@@ -0,0 +1,15 @@
+class Child {
+	
+	//use geoLite lookup service to get this
+	Location childLocation;
+	String name;
+	
+	//may have to change name of function to match that in parent class
+	Location showLocation (
+		updateLocation();
+		return Location;
+	}
+	
+	String getName () {return name;};
+	String setName(String nm) {name = nm;};
+}
